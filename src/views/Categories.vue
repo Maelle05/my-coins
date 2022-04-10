f0e8ba6ef3932d4073f5506bcf5800a1f796ea2c
Maelle05 <maelle.rabouan@gmail.com>
2022-04-10 14:49:18 +0200

    feat(): show cat icon

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="Categories">
-    <h1>Categories</h1>
+    <h1>Categories </h1>
+    <div >
+      <img v-for="index in total_icons_cat" v-bind:key="index" :src="getImgUrl(index-1)" alt="" srcset="" >
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'App',
+  data(){
+    return {
+      total_icons_cat: require.context('../assets/categories/', false, /\.(svg)$/).keys().length,
+      icons: require.context('../assets/categories/', false, /\.svg$/).keys().map(require.context('../assets/categories/', false, /\.svg$/))
+    }
+  },
+  mounted(){
+  },
+  methods: {
+    getImgUrl(i) {
+      return this.icons[i]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+img{
+  width: 30px;
+  height: 30px;
+}
 </style>
