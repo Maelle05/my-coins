bede6a2a2b945632a879021b60ad684dd3e3cb2e
Maelle05 <maelle.rabouan@gmail.com>
2022-04-15 11:24:54 +0200

    change time alert

--- src/components/form/InputNum.vue
+++ src/components/form/InputNum.vue
@@ -2,7 +2,7 @@
   <div class="InputNum">
     <label ref="label" :for="name" class="" :class="{ disabled: disabled, 'out': value }">{{label}}</label>
     <input ref="input" step=".01" type="number" :id="name" :value="value ? value : ''" :disabled="disabled" :class="disabled ? 'disabled' : '' " />
-    <span :class="disabled ? 'disabled' : '' " >{{user.data.currency}}</span>
+    <span :class="disabled ? 'disabled' : '' ">{{user.data.currency}}</span>
   </div>
 </template>
 
